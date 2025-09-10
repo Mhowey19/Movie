21f0623845ba3a2113219b33338c424e825da0fb
Mhowey19 <michaelhowey5@gmail.com>
2025-09-10 15:16:06 -0400

    Add the movie list functionality

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.listen(3000, () => {
 });
 
 //create a function that converts the movie name to the movie id using search
-//make a .env file for my api key
+
 const api = process.env.apiKey;
 
-//json:true parses the json data
+//Shows the movies similar to the movie picked
 const movieSimilar = (movieId) => {
 	const url = `https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`;
 	const options = {
@@ -26,10 +26,31 @@ const movieSimilar = (movieId) => {
 			Authorization: `Bearer ${api}`,
 		},
 	};
-
+	//data will be injected into the data similar
 	fetch(url, options)
 		.then((res) => res.json())
 		.then((json) => console.log(json))
 		.catch((err) => console.error(err));
 };
-movieSimilar(242);
+// movieSimilar(242);
+
+//Will house the movies name and shows a list of the apis linked to that movie
+const movieList = (movie) => {
+	const url = `https://api.themoviedb.org/3/search/movie?query=${movie}&include_adult=false&language=en-US&page=1`;
+
+	const options = {
+		method: "GET",
+		headers: {
+			accept: "application/json",
+			Authorization: `Bearer ${api}`,
+		},
+	};
+
+	//data would be injected into the body html for movie list
+	fetch(url, options)
+		.then((res) => res.json())
+		.then((json) => console.log(json))
+		.catch((err) => console.error(err));
+};
+
+movieList("Cars");
